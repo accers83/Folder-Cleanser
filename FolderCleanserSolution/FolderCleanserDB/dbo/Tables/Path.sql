fa0296a5b11b348ee1412c67b668db4d6bb50422
Chris Accleton-Pennell <Chris.AccletonPennell@fxplus.ac.uk>
2024-11-18 07:01:25 +0000

    Created FolderCleanser database

---
+++ FolderCleanserSolution/FolderCleanserDB/dbo/Tables/Path.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Path]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Path] NVARCHAR(500) NOT NULL, 
+    [RetentionDays] INT NOT NULL, 
+    [Created] DATETIME2 NOT NULL DEFAULT getdate(), 
+    [Deleted] DATETIME2 NULL
+)
---
+++ FolderCleanserSolution/FolderCleanserDB/dbo/Tables/SummaryHistory.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[SummaryHistory]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [PathId] INT NOT NULL, 
+    [ProcessingStartDateTime] DATETIME2 NOT NULL, 
+    [ProcessingEndDataTime] DATETIME2 NOT NULL, 
+    [ProcessingDurationMins] INT NOT NULL DEFAULT 0, 
+    [FilesDeletedCount] INT NOT NULL DEFAULT 0, 
+    [FileSizeDeletedBytes] INT NOT NULL DEFAULT 0
+)
