9e4dfdcf607ec1022f53d34cfa357e6cdff6b306
Chris Accleton-Pennell <Chris.AccletonPennell@fxplus.ac.uk>
2025-01-04 22:17:44 +0000

    Added first unit test for ConvertBytesToMB method in ConversionHelper

---
+++ FolderCleanserSolution/FolderCleanserFrontEndLibrary.Tests/ConversionHelperTests.cs
@@ -0,0 +1,22 @@
+﻿using FolderCleanserFrontEndLibrary.Helpers;
+
+namespace FolderCleanserFrontEndLibrary.Tests;
+
+public class ConversionHelperTests
+{
+    [Fact]
+    public void ConvertBytesToMb_Should_Return_Zero()
+    {
+        // Arrange
+        var conversionHelper = new ConversionHelper();
+
+        long bytes = 0;
+        double expected = 0;
+
+        // Act 
+        double actual = conversionHelper.ConvertBytesToMB(bytes);
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+}
