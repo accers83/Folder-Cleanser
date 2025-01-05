bfbdca67c5464b3bd76482293d25e0f830a183a7
Chris Accleton-Pennell <Chris.AccletonPennell@fxplus.ac.uk>
2025-01-05 21:21:15 +0000

    Added unit tests for ConvertBytesToMb method in ConversionHelper, FrontEndLibrary project

--- FolderCleanserSolution/FolderCleanserFrontEndLibrary.Tests/ConversionHelperTests.cs
+++ FolderCleanserSolution/FolderCleanserFrontEndLibrary.Tests/ConversionHelperTests.cs
@@ -5,13 +5,44 @@ namespace FolderCleanserFrontEndLibrary.Tests;
 public class ConversionHelperTests
 {
     [Fact]
-    public void ConvertBytesToMb_Should_Return_Zero()
+    public void When_Bytes_Zero_ConvertBytesToMb_Should_Return_Zero()
+    {
+        // Arrange
+        var conversionHelper = new ConversionHelper();
+        long bytes = 0;
+        double expected = 0;
+
+        // Act 
+        double actual = conversionHelper.ConvertBytesToMB(bytes);
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData(-1)]
+    [InlineData(-1001)]
+    [InlineData(long.MinValue)]
+    public void When_Bytes_Negative_ConvertBytesToMb_Should_Throw_Exception(long bytes)
     {
         // Arrange
         var conversionHelper = new ConversionHelper();
 
-        long bytes = 0;
-        double expected = 0;
+        // Act 
+        Action convertBytesToMb = () => conversionHelper.ConvertBytesToMB(bytes);
+
+        // Assert
+        Assert.ThrowsAny<ArgumentOutOfRangeException>(convertBytesToMb);
+    }
+
+    [Theory]
+    [InlineData(1, 9.5367431640625E-07)]
+    [InlineData(1000000, 0.95367431640625)]
+    [InlineData(long.MaxValue, 8796093022208)]
+    public void When_Bytes_Positive_ConvertBytesToMb_Should_Return_Value(long bytes, double expected)
+    {
+        // Arrange
+        var conversionHelper = new ConversionHelper();
 
         // Act 
         double actual = conversionHelper.ConvertBytesToMB(bytes);
--- FolderCleanserSolution/FolderCleanserFrontEndLibrary/Helpers/ConversionHelper.cs
+++ FolderCleanserSolution/FolderCleanserFrontEndLibrary/Helpers/ConversionHelper.cs
@@ -4,6 +4,11 @@ public class ConversionHelper : IConversionHelper
 {
     public double ConvertBytesToMB(long bytes)
     {
+        if (bytes < 0)
+        {
+            throw new ArgumentOutOfRangeException("bytes", "Parameter must >= 0");
+        }
+        
         double output = 0;
 
         output = ((double)bytes / 1024) / 1024;
